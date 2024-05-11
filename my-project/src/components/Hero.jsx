e7587b401ad7f28af8eacee69fd1227cfe2a1ac6
Sujeeththecoder <sujeethgorati08@gmail.com>
2024-05-11 09:58:38 +0530

    Complete navbar without styling

---
+++ my-project/src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function Hero() {
+  return (
+    <div className='text-white'>
+      <div className='w-full text-center items-center mt-40 mb-30 h-screen mx-auto flex flex-col text-bold'>
+<div className='font-bold text-xl text-sky-400 sm:text-4xl'>
+  <h1 className='animate-pulse mb-4 m-auto'>Find Your Teacher Based On Your Budget</h1>
+
+  <button className=' text-lg bg-blue-500 text-white py-1 px-4 border border-blue-500 hover:border-transparent rounded'>Book A Free Session Now</button>
+
+
+  
+
+</div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero;
